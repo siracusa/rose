6e2695ec0f281695b9a2630bc335cabd115c2fd2
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2005-10-17 14:57:36 +0000

    More stubs.

--- trunk/rose/modules/Rose-DB-Object/lib/Rose/DB/Object/ConventionManager/Null.pm
+++ trunk/rose/modules/Rose-DB-Object/lib/Rose/DB/Object/ConventionManager/Null.pm
@@ -7,7 +7,15 @@ our @ISA = qw(Rose::DB::Object::ConventionManager);
 
 our $VERSION = '0.01';
 
-sub auto_table_name { }
-sub auto_primary_key_column_names { }
+sub class_to_table_singular { }
+sub class_to_table_plural { }
+sub table_to_class { }
+sub auto_table_name  { }
+sub auto_primary_key_column_names  { }
+sub default_singular_to_plural  { }
+sub singular_to_plural  { }
+sub auto_foreign_key  { }
+sub auto_relationship  { }
+sub auto_relationship_one_to_one { }
 
 1;
