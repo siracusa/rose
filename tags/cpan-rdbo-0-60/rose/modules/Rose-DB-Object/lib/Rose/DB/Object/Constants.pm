3d914c27b3b3ea57c2408b9bf6480e5793d2d8be
nobody <nobody@59ea1863-d93a-0410-9336-277072e1bf86>
2006-01-01 01:33:01 +0000

    This commit was manufactured by cvs2svn to create tag 'cpan-rdbo-0-60'.

---
+++ tags/cpan-rdbo-0-60/rose/modules/Rose-DB-Object/lib/Rose/DB/Object/Constants.pm
@@ -0,0 +1,27 @@
+package Rose::DB::Object::Constants;
+
+use strict;
+
+our $VERSION = '0.01';
+
+require Exporter;
+our @ISA = qw(Exporter);
+
+our @EXPORT_OK = 
+  qw(PRIVATE_PREFIX META_ATTR_NAME ON_SAVE_ATTR_NAME 
+     LOADED_FROM_DRIVER FLAG_DB_IS_PRIVATE
+     STATE_IN_DB STATE_LOADING STATE_SAVING STATE_CLONING);
+
+our %EXPORT_TAGS = (all => \@EXPORT_OK);
+
+use constant PRIVATE_PREFIX     => '__xrdbopriv';
+use constant META_ATTR_NAME     => PRIVATE_PREFIX . '_meta';
+use constant ON_SAVE_ATTR_NAME  => PRIVATE_PREFIX . '_on_save';
+use constant LOADED_FROM_DRIVER => PRIVATE_PREFIX . '_loaded_from_driver';
+use constant FLAG_DB_IS_PRIVATE => PRIVATE_PREFIX . '_db_is_private';
+use constant STATE_IN_DB        => PRIVATE_PREFIX . '_in_db';
+use constant STATE_LOADING      => PRIVATE_PREFIX . '_loading';
+use constant STATE_SAVING       => PRIVATE_PREFIX . '_saving';
+use constant STATE_CLONING      => STATE_SAVING;
+
+1;
