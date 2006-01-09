1dc8f7535e9f1c4baebb400727d9d4fd1f3f2d91
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2006-01-09 16:38:13 +0000

    *** empty log message ***

--- rose/modules/Rose-HTML-Objects/lib/Rose/HTML/Form/Field/DateTime.pm
+++ rose/modules/Rose-HTML-Objects/lib/Rose/HTML/Form/Field/DateTime.pm
@@ -8,7 +8,7 @@ use Rose::DateTime::Parser;
 use Rose::HTML::Form::Field::Text;
 our @ISA = qw(Rose::HTML::Form::Field::Text);
 
-our $VERSION = '0.012';
+our $VERSION = '0.33';
 
 use Rose::Object::MakeMethods::Generic
 (
@@ -61,6 +61,12 @@ sub validate
 
   my $date = $self->internal_value;
   return 1  if(ref $date eq 'DateTime');
+
+  if($self->has_partial_value)
+  {
+    $self->error('Incomplete value');
+    return 0;
+  }
 
   my $input = $self->input_value_filtered;
   return 1  unless(length $input);
