5325f1d05dd733d963e246768a5c0fe3dff729b0
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2006-02-17 05:30:05 +0000

    Updated copyright date

--- rose/modules/Rose-Object/lib/Rose/Class/MakeMethods/Set.pm
+++ rose/modules/Rose-Object/lib/Rose/Class/MakeMethods/Set.pm
@@ -978,4 +978,4 @@ John C. Siracusa (siracusa@mindspring.com)
 
 =head1 COPYRIGHT
 
-Copyright (c) 2005 by John C. Siracusa.  All rights reserved.  This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+Copyright (c) 2006 by John C. Siracusa.  All rights reserved.  This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
