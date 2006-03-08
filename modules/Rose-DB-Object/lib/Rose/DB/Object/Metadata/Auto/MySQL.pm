9cfddff823bab04b6d722d39421de0b048e9f63c
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2006-03-08 13:01:23 +0000

    Patch from Uwe Voelker

--- modules/Rose-DB-Object/lib/Rose/DB/Object/Metadata/Auto/MySQL.pm
+++ modules/Rose-DB-Object/lib/Rose/DB/Object/Metadata/Auto/MySQL.pm
@@ -90,7 +90,7 @@ sub auto_generate_unique_keys
   # This sort order is part of the API, and is essential to make the
   # test suite work.
   no warnings 'uninitialized';
-  my @uk = map { $unique_keys{$_} } sort { lc $a <=> lc $b } keys(%unique_keys);
+  my @uk = map { $unique_keys{$_} } sort { lc $a cmp lc $b } keys(%unique_keys);
 
   return wantarray ? @uk : \@uk;
 }
