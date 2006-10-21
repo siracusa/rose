7223a3ba93eede27dabe7ea2ad50706203421112
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2006-10-21 00:11:11 +0000

    Rose::DB::Object 0.755 tag

---
+++ tags/cpan-rdbo-0-755/modules/Rose-DB-Object/t/benchmarks/lib/MyTest/DBIC/Simple/Product.pm
@@ -0,0 +1,23 @@
+package MyTest::DBIC::Simple::Product;
+
+use strict;
+
+use MyTest::DBIC::Simple::Code;
+use MyTest::DBIC::Complex::CodeName;
+use MyTest::DBIC::Simple::Category;
+
+use base 'MyTest::DBIC::Base';
+
+__PACKAGE__->table('rose_db_object_test_products');
+__PACKAGE__->add_columns(qw(category_id date_created fk1 fk2 fk3 id last_modified name published status));
+__PACKAGE__->set_primary_key('id');
+
+__PACKAGE__->add_relationship('category_id', 'MyTest::DBIC::Simple::Category',
+                              { 'foreign.id' => 'self.category_id' },
+                              { accessor => 'filter' });
+
+__PACKAGE__->add_relationship('code_names', 'MyTest::DBIC::Simple::CodeName',
+                              { 'foreign.product_id' => 'self.id' },
+                              { accessor => 'multi', join_type => 'LEFT OUTER' });
+
+1;
