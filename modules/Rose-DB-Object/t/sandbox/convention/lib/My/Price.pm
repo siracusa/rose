58158cb23f51539f76ef7dca9d0f94f11571b1f1
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2006-10-31 01:02:44 +0000

    Rose::DB 0.729 tag

--- modules/Rose-DB-Object/t/sandbox/convention/lib/My/Price.pm
+++
@@ -1,7 +0,0 @@
-package My::Price;
-use strict;
-use base 'My::Object';
-__PACKAGE__->meta->columns(qw(price_id product_id region price));
-__PACKAGE__->meta->foreign_keys(qw(product));
-__PACKAGE__->meta->initialize;
-1;
