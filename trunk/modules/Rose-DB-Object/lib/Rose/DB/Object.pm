5b3b5580f5e9d75eda470f77bda032852fc300a5
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2008-10-03 15:45:33 +0000

    RDBO 0.773

--- trunk/modules/Rose-DB-Object/lib/Rose/DB/Object.pm
+++ trunk/modules/Rose-DB-Object/lib/Rose/DB/Object.pm
@@ -16,7 +16,7 @@ use Rose::DB::Constants qw(IN_TRANSACTION);
 use Rose::DB::Object::Exception;
 use Rose::DB::Object::Util();
 
-our $VERSION = '0.7722_01';
+our $VERSION = '0.773';
 
 our $Debug = 0;
 
