4570d57ea3aa24256613589e5ec0b4d60140ff01
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2010-05-04 20:54:14 +0000

    Fixed SQL reserved word quoting bug (needs tests)

--- trunk/modules/Rose-DB-Object/lib/Rose/DB/Object.pm
+++ trunk/modules/Rose-DB-Object/lib/Rose/DB/Object.pm
@@ -16,7 +16,7 @@ use Rose::DB::Constants qw(IN_TRANSACTION);
 use Rose::DB::Object::Exception;
 use Rose::DB::Object::Util();
 
-our $VERSION = '0.787_01';
+our $VERSION = '0.787_02';
 
 our $Debug = 0;
 
