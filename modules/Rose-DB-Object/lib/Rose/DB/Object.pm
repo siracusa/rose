85bec65b2074cbf30d1ca81b459c6f6ab1ebcf7b
siracusa <siracusa@59ea1863-d93a-0410-9336-277072e1bf86>
2010-06-23 18:59:14 +0000

    CPAN Rose-DB-Object 0.789

--- modules/Rose-DB-Object/lib/Rose/DB/Object.pm
+++ modules/Rose-DB-Object/lib/Rose/DB/Object.pm
@@ -16,7 +16,7 @@ use Rose::DB::Constants qw(IN_TRANSACTION);
 use Rose::DB::Object::Exception;
 use Rose::DB::Object::Util();
 
-our $VERSION = '0.788_03';
+our $VERSION = '0.789';
 
 our $Debug = 0;
 
